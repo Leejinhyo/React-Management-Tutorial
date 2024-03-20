62fb932bb7a28ffcfe2b791a5b49a9d3beef8c40
ljh <jinhyo6407@naver.com>
2024-03-20 16:27:41 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    debugger;
-    res.send([
-      {
-        id: 1,
-        image: "https://picsum.photos/id/1/64/64",
-        name: "홍길동",
-        birthday: "900122",
-        gender: "남자",
-        job: "대학생",
-      },
-      {
-        id: 2,
-        image: "https://picsum.photos/id/2/64/64",
-        name: "홍길동2",
-        birthday: "910122",
-        gender: "남자2",
-        job: "대학생2",
-      },
-      {
-        id: 3,
-        image: "https://picsum.photos/id/3/64/64",
-        name: "홍길동3",
-        birthday: "93333",
-        gender: "남자3",
-        job: "대학생3",
-      },
-    ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
