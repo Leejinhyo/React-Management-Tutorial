f5b1eec3621bba4b01611d308ff6baf0247fa24c
ljh <jinhyo6407@naver.com>
2024-03-20 14:01:36 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 import { Paper, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 
-const customers = [
-  {
-    id: 1,
-    image: "https://picsum.photos/id/1/64/64",
-    name: "홍길동",
-    birthday: "900122",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 2,
-    image: "https://picsum.photos/id/2/64/64",
-    name: "홍길동2",
-    birthday: "910122",
-    gender: "남자2",
-    job: "대학생2",
-  },
-  {
-    id: 3,
-    image: "https://picsum.photos/id/3/64/64",
-    name: "홍길동3",
-    birthday: "93333",
-    gender: "남자3",
-    job: "대학생3",
-  },
-];
-
 function App() {
+
+  let [customers, setCustomers] = useState("");
+
+  const fetchData = async () => {
+    await fetch('/api/customers')
+      .then(res=>res.json())
+      .then(result=> {
+        setCustomers(result);
+      });
+  };
+
+  useEffect( ()=> {
+    fetchData();
+  }, []);
+
   
   return (
-    <Paper >
-      <Table >
+    <Paper>
+      <Table>
         <TableHead>
           <TableRow>
             <TableCell>번호</TableCell>
@@ -45,20 +34,19 @@ function App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers.map((customer) => (
-            <Customer
-              key={customer.id}
-              id={customer.id}
-              image={customer.image}
-              name={customer.name}
-              birthday={customer.birthday}
-              gender={customer.gender}
-              job={customer.job}
-            />
+          {customers && 
+            customers.map((customer) => (
+              <Customer
+                key={customer.id}
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+              />
           ))}
-
         </TableBody>
-        
       </Table>
     </Paper>
   );
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express'});
+app.get('/api/customers', (req, res) => {
+    debugger;
+    res.send([
+      {
+        id: 1,
+        image: "https://picsum.photos/id/1/64/64",
+        name: "홍길동",
+        birthday: "900122",
+        gender: "남자",
+        job: "대학생",
+      },
+      {
+        id: 2,
+        image: "https://picsum.photos/id/2/64/64",
+        name: "홍길동2",
+        birthday: "910122",
+        gender: "남자2",
+        job: "대학생2",
+      },
+      {
+        id: 3,
+        image: "https://picsum.photos/id/3/64/64",
+        name: "홍길동3",
+        birthday: "93333",
+        gender: "남자3",
+        job: "대학생3",
+      },
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
